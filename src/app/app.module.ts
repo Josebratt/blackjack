bbc902c393e8f1f2aca649bc356f768f310c657e
Josebratt <josebratt@gmail.com>
2022-05-11 16:37:14 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home/home.component';
+import { StartComponent } from './start/start.component';
 
 @NgModule({
   declarations: [
-    HomeComponent
+    HomeComponent,
+    StartComponent
   ],
   imports: [
     BrowserModule
