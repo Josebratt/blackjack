2f3219b80122de12fc8b2cdac289b8efae021fc6
Josebratt <josebratt@gmail.com>
2022-05-12 12:00:12 -0400

    Update start.component.ts

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ConstantsService } from '../_services/constants.service';
 
 @Component({
   selector: 'app-start',
@@ -7,9 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StartComponent implements OnInit {
 
-  constructor() { }
+  constructor(public constantsService: ConstantsService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void {    
   }
 
 }
