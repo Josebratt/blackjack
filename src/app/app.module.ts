8edd694865cde0e1b899c44ea145680f31ced76a
Josebratt <josebratt@gmail.com>
2022-05-12 11:59:25 -0400

    Update app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { HomeComponent } from './home/home.component';
 import { StartComponent } from './start/start.component';
+import { EndComponent } from './end/end.component';
+import { GameComponent } from './game/game.component';
+import { ControlsComponent } from './controls/controls.component';
 
 @NgModule({
   declarations: [
     HomeComponent,
-    StartComponent
+    StartComponent,
+    EndComponent,
+    GameComponent,
+    ControlsComponent
   ],
   imports: [
     BrowserModule
