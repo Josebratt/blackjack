bae3f98ee209f18b1bf839012c55d712de119434
Josebratt <josebratt@gmail.com>
2022-05-18 11:09:14 -0400

    Update controls.component.ts

--- src/app/controls/controls.component.ts
+++ src/app/controls/controls.component.ts
@@ -1,27 +1,16 @@
-import { Component, OnInit, OnChanges, SimpleChanges, DoCheck, OnDestroy } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-controls',
   templateUrl: './controls.component.html',
   styleUrls: ['./controls.component.scss']
 })
-export class ControlsComponent implements OnInit, OnChanges, DoCheck, OnDestroy {
+export class ControlsComponent implements OnInit {
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
-  ngOnChanges(changes: SimpleChanges): void {
-    throw new Error('Method not implemented.');
-  }
-
-  ngDoCheck(): void {
-    throw new Error('Method not implemented.');
-  }
-
-  ngOnDestroy(): void {
-    throw new Error('Method not implemented.');
-  }
 
 }
