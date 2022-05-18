8b16f4c688b1128440115b2632e318f896ccdbbb
Josebratt <josebratt@gmail.com>
2022-05-18 11:09:32 -0400

    Update home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core';
+import { ThisReceiver } from '@angular/compiler';
+import { Component, OnInit } from '@angular/core';
+import { Deck } from '../_models/deck';
+import { Player } from '../_models/player';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   title = 'blackjack';
+
+  // hideStart = false;
+  // hideGame = true;
+  // hideEnd = true;
+
+  gameStarted = false;
+  gameEnded = false;
+
+  ngOnInit(): void {
+    const deck = new Deck(false);
+    deck.shuffle();
+
+    const players = [];
+    players.push(new Player('Dealer'));
+    players.push(new Player('Dan'));
+
+    deck.deal(players, 2);
+
+    // console.log(players[1]);
+  }
+
+  startGame() {
+    this.gameStarted = true;
+    this.gameEnded = false;
+    // this.hideStart = true;
+    // this.hideEnd = false;
+  }
 }
