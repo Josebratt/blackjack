4fcc99be301aa5561c24698570764adb325c9405
Josebratt <josebratt@gmail.com>
2022-05-18 11:09:10 -0400

    Update constants.service.ts

--- src/app/_services/constants.service.ts
+++ src/app/_services/constants.service.ts
@@ -9,10 +9,20 @@ export class ConstantsService {
   public FACES: string[];
   public PIPS: number[];
 
+  public PLAYER_TITLE: string;
+  public PLAYER_NAME_LABEL: string;
+  public ACES_HIGH_LABEL: string;
+  public START_LABEL: string;
+
   constructor() {
     this.COLORS = ['Black', 'Red'];
     this.SUITS = ['Spades', 'Hearts', 'Diamonds', 'Clubs'];
     this.FACES = ['Aces', 'Jack', 'Queen', 'King'];
     this.PIPS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10];
+
+    this.PLAYER_TITLE = 'Play 21 or bust! Beat the dealer to win big';
+    this.PLAYER_NAME_LABEL = 'Player name:';
+    this.ACES_HIGH_LABEL = 'Aces high?:';
+    this.START_LABEL = 'Start!';
   }
 }
