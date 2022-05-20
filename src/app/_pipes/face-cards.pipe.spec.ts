4792cea1d73dfdc4bd72a75034ac5a71bf4e452f
Josebratt <josebratt@gmail.com>
2022-05-20 10:06:58 -0400

    Create face-cards.pipe.spec.ts

---
+++ src/app/_pipes/face-cards.pipe.spec.ts
@@ -0,0 +1,8 @@
+import { FaceCardsPipe } from './face-cards.pipe';
+
+describe('FaceCardsPipe', () => {
+  it('create an instance', () => {
+    const pipe = new FaceCardsPipe();
+    expect(pipe).toBeTruthy();
+  });
+});
