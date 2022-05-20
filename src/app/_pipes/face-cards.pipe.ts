1e7d0915babc98ca3a8a37f5c54d73ee4ec9f63c
Josebratt <josebratt@gmail.com>
2022-05-20 14:32:08 -0400

    Update face-cards.pipe.ts

--- src/app/_pipes/face-cards.pipe.ts
+++ src/app/_pipes/face-cards.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
+import { Card } from '../_models/card';
 
 @Pipe({
   name: 'faceCards'
 })
 export class FaceCardsPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(cards: Card[], easyMode: boolean): Card[] {
+    if (!easyMode) {
+      return cards;
+    }
+    return cards.filter(card => card.value < 10);
   }
 
 }
