f920f98b1b5d2053971a469e141d3c453f258802
Josebratt <josebratt@gmail.com>
2022-05-21 13:42:45 -0400

    Update start.component.ts

--- src/app/start/start.component.ts
+++ src/app/start/start.component.ts
@@ -1,4 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { FormGroup } from '@angular/forms';
 import { ConstantsService } from '../_services/constants.service';
 
 @Component({
@@ -13,31 +14,26 @@ export class StartComponent implements OnInit {
   @Output()
   start = new EventEmitter<string>();
 
-  disableButton: boolean = false;
   acesHigh: boolean = false;
   easyMode: boolean = false;
-  states!: object;
   playerName: string = '';
 
   constructor(public constantsService: ConstantsService) {}
 
   ngOnInit(): void {
-    this.disableButton = false;
-
-    this.states = {
-      warning: true,
-      error: false,
-    };
   }
 
-  startGame(event: any): void {
-    event.preventDefault();
-    let emitData: any = {
-      playerName: this.playerName,
-      acesHigh: this.acesHigh,
-      easyMode: this.easyMode
+  startGame(playerForm: FormGroup): void {
+    if (playerForm.valid) {
+      let emitData: any = {
+        playerName: this.playerName,
+        acesHigh: this.acesHigh,
+        easyMode: this.easyMode,
+      };
+      this.start.emit(emitData);
+    } else {
+      playerForm.controls.name.markAsTouched();
     }
-    this.start.emit(emitData);
   }
 
   toggleAces(): void {
